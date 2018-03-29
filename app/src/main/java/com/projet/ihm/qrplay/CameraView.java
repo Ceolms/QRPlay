e120acbcbfe5bfa712b9e7bca6eb0ec90025c9b6
Unknown <theo@gaerel.net>
2018-03-29 16:08:48 +0200

    Debut CameraView

--- app/src/main/java/com/projet/ihm/qrplay/CameraView.java
+++ app/src/main/java/com/projet/ihm/qrplay/CameraView.java
@@ -1,10 +1,83 @@
 package com.projet.ihm.qrplay;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
-/**
- * Created by theo on 29/03/2018.
- */
+import com.google.zxing.ResultPoint;
+import com.google.zxing.client.android.BeepManager;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+
+import java.util.List;
 
 public class CameraView extends Activity {
+
+    private static final String TAG = CameraView.class.getSimpleName();
+   // private DecoratedBarcodeView barcodeView;
+    private BeepManager beepManager;
+    private String lastText;
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if(result.getText() == null || result.getText().equals(lastText)) {
+                // Prevent duplicate scans
+                return;
+            }
+
+            lastText = result.getText();
+           // barcodeView.setStatusText(result.getText());
+
+            beepManager.playBeepSoundAndVibrate();
+
+            //Added preview of scanned barcode
+           // ImageView imageView = (ImageView) findViewById(R.id.barcodePreview);
+          // imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW));
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_camera_view);
+/*
+        barcodeView = (DecoratedBarcodeView) findViewById(R.id.barcode_scanner);
+        Collection<BarcodeFormat> formats = Arrays.asList(BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39);
+        barcodeView.getBarcodeView().setDecoderFactory(new DefaultDecoderFactory(formats));
+        barcodeView.decodeContinuous(callback);
+*/
+        beepManager = new BeepManager(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+      //  barcodeView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    //    barcodeView.pause();
+    }
+
+    public void pause(View view) {
+     //   barcodeView.pause();
+    }
+
+    public void resume(View view) {
+      //  barcodeView.resume();
+    }
+
+    public void triggerScan(View view) {
+      //  barcodeView.decodeSingle(callback);
+    }
 }
--- app/src/main/java/com/projet/ihm/qrplay/MainMenu.java
+++ app/src/main/java/com/projet/ihm/qrplay/MainMenu.java
@@ -1,7 +1,9 @@
 package com.projet.ihm.qrplay;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(MainMenu.this, CameraView.class);
+        startActivity(intent);
     }
 }
