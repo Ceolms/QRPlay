fa864abc5407c8e6ec51f7ba05bfdbf1d20d0554
StephaneGosset <stephane.gosset@u-psud.fr>
2018-04-05 16:40:05 +0200

    Imprimer QR Codes avec apercu

--- app/src/main/java/com/projet/ihm/qrplay/PrintView.java
+++ app/src/main/java/com/projet/ihm/qrplay/PrintView.java
@@ -1,12 +1,23 @@
 package com.projet.ihm.qrplay;
 
-import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.print.PrintHelper;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class PrintView extends Activity{
+public class PrintView extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_print_view);
+    }
+
+    public void PrintCodes(View view) {
+        PrintHelper p = new PrintHelper(this);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.qr_codes);
+        p.printBitmap("QR Codes", bitmap);
     }
 }
