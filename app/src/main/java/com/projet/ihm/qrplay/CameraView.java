d59ba5f91c2b9db2fe09484b57fee7d18ef37eef
Unknown <theo@gaerel.net>
2018-04-05 15:56:31 +0200

    Lecture QR Code fonctionnel ( a optimiser)

--- app/src/main/java/com/projet/ihm/qrplay/CameraView.java
+++ app/src/main/java/com/projet/ihm/qrplay/CameraView.java
@@ -1,14 +1,83 @@
 package com.projet.ihm.qrplay;
 
-import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
 
-public class CameraView extends Activity {
+import com.google.android.gms.samples.vision.barcodereader.BarcodeCapture;
+import com.google.android.gms.samples.vision.barcodereader.BarcodeGraphic;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.List;
+
+import xyz.belvi.mobilevisionbarcodescanner.BarcodeRetriever;
+
+public class CameraView extends AppCompatActivity implements BarcodeRetriever {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_view);
+
+
+        final BarcodeCapture barcodeCapture = (BarcodeCapture) getSupportFragmentManager().findFragmentById(R.id.barcode);
+        barcodeCapture.setRetrieval(this);
+        barcodeCapture.setShowDrawRect(true);
+
     }
 
+
+    @Override
+    public void onPermissionRequestDenied() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(CameraView.this)
+                .setTitle("Erreur")
+                .setMessage("La camera est néccéssaire!");
+        builder.show();
+    }
+
+    // for one time scan
+    @Override
+    public void onRetrieved(final Barcode barcode) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(CameraView.this)
+                        .setTitle("code retrieved")
+                        .setMessage(barcode.displayValue);
+                builder.show();
+            }
+        });
+
+    }
+    // for multiple callback
+    @Override
+    public void onRetrievedMultiple(final Barcode closetToClick, final List<BarcodeGraphic> barcodeGraphics) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String message = "Code selected : " + closetToClick.displayValue + "\n\nother " +
+                        "codes in frame include : \n";
+                for (int index = 0; index < barcodeGraphics.size(); index++) {
+                    Barcode barcode = barcodeGraphics.get(index).getBarcode();
+                    message += (index + 1) + ". " + barcode.displayValue + "\n";
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(CameraView.this)
+                        .setTitle("code retrieved")
+                        .setMessage(message);
+                builder.show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+        // when image is scanned and processed
+    }
+
+    @Override
+    public void onRetrievedFailed(String reason) {
+        // in case of failure
+    }
 }
