ff93977d39a9386a81fd36ed82cd9d946aaf39c3
StephaneGosset <stephane.gosset@u-psud.fr>
2018-04-12 18:39:25 +0200

    Partage des QR codes

--- app/src/main/java/com/projet/ihm/qrplay/PrintView.java
+++ app/src/main/java/com/projet/ihm/qrplay/PrintView.java
@@ -1,23 +1,71 @@
 package com.projet.ihm.qrplay;
 
+import java.io.*;
+
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.v4.content.FileProvider;
 import android.support.v4.print.PrintHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ShareActionProvider;
+
 
 public class PrintView extends AppCompatActivity{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_print_view);
     }
 
-    public void PrintCodes(View view) {
+    private ShareActionProvider mShareActionProvider;
+
+    //imprimer les codes directement
+    public void PrintCodes(View view){
         PrintHelper p = new PrintHelper(this);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
-                R.drawable.qr_codes);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.raw.qr_codes);
         p.printBitmap("QR Codes", bitmap);
+    }
+
+    //envoyer les codes avec une autre application
+    public void sendCodes(View view){
+        //préparer le fichier
+        try {
+        File dir = new File(getApplicationContext().getCacheDir(), "qr_print");
+        if(!dir.exists()) //noinspection ResultOfMethodCallIgnored
+            dir.mkdirs();
+        File image = new File(dir, "qr_codes.png");
+        if(!image.exists()){
+            //copier le fichier depuis les ressources
+            InputStream in = getResources().openRawResource(R.raw.qr_codes);
+            OutputStream out = new FileOutputStream(image);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            out.flush();
+            out.close();
+            in.close();
+        }
+        Uri imageUri = FileProvider.getUriForFile(getApplicationContext(), "com.projet.ihm.qrplay.fileprovider", image);
+
+        //envoyer le fichier
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+        shareIntent.setType("image/png");
+        startActivity(Intent.createChooser(shareIntent, getString(R.string.share)));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
